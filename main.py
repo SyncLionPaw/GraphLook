e9fca0dabf7d22492df3b62c09f119590ecbc716
YuleiGong <gongyulei@stu.xmu.edu.cn>
2025-03-25 16:05:17 +0800

    fix token leak

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import asyncio
+import os
 from app.llm import LLM, LLMSettings
-from app.agent.graphlook import BaseAgent
+from app.agent.base import BaseAgent
 
 
 async def test_base_agent():
     llm_settings = LLMSettings(
         model="qwen-plus",
         base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
-        api_key="sk-a00f8cd1ceeb46018027d05fcdf4e898",
+        api_key=os.getenv("API_KEY") or "",
     )
     llm = LLM(llm_config=llm_settings)
     bs_agent = BaseAgent("test1", "a simple agent", llm)
